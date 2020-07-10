424d3a2f45bb2a0092bbce5b7dbba88631b36d7a
Jason Sebastian <jaritant@sfu.ca>
2020-07-10 09:21:41 -0700

    add getInstance

--- app/src/main/java/com/example/finddamatch/MainActivity.java
+++ app/src/main/java/com/example/finddamatch/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Deck.getInstance();
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
         /**

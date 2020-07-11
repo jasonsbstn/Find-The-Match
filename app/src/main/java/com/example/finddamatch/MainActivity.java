fa3bbe2baaff8fd31d433af95191c184609c7219
Sehajvir Pannu <sspannu@sfu.ca>
2020-07-10 17:11:34 -0700

    Resolve merge conflicts

--- app/src/main/java/com/example/finddamatch/MainActivity.java
+++ app/src/main/java/com/example/finddamatch/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.example.finddamatch.Classes.cards;
 import com.example.finddamatch.Classes.deck;
 import com.example.finddamatch.Classes.discard;
+import com.example.finddamatch.UI.Main_Menu;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 

c1ab4fb8c88539f16eabd2fbbb45707ed8b32ce0
Gursmeep <gsyan@sfu.ca>
2020-07-11 19:43:24 -0700

    Initial Gameplay Commit

---
+++ app/src/main/java/com/example/finddamatch/GamePlay/Logic.java
@@ -0,0 +1,25 @@
+package com.example.finddamatch.GamePlay;
+
+import com.example.finddamatch.Classes.deck;
+import com.example.finddamatch.Classes.cards;
+import com.example.finddamatch.Classes.discard;
+import com.example.finddamatch.Cards.card1;
+import com.example.finddamatch.Cards.card2;
+import com.example.finddamatch.Cards.card3;
+import com.example.finddamatch.Cards.card4;
+import com.example.finddamatch.Cards.card5;
+import com.example.finddamatch.Cards.card6;
+import com.example.finddamatch.Cards.card7;
+
+
+public class Logic {
+    deck deck1;
+
+
+
+
+
+
+}
+
+

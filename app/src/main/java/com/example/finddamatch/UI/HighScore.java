5744d5c9cd7a95e866ac6a9488955d26194c9872
xtran <xuan_anh_tran@sfu.ca>
2020-07-14 11:10:54 +0700

    username for highscore

--- app/src/main/java/com/example/finddamatch/UI/HighScore.java
+++ app/src/main/java/com/example/finddamatch/UI/HighScore.java
@@ -10,7 +10,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import com.example.finddamatch.R;

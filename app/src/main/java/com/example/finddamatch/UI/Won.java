639e1a45cedce52f539c2a7cee906964b141674e
xtran <xuan_anh_tran@sfu.ca>
2020-07-14 10:02:10 +0700

    timestamp for highscore

--- app/src/main/java/com/example/finddamatch/UI/Won.java
+++ app/src/main/java/com/example/finddamatch/UI/Won.java
@@ -6,11 +6,17 @@ import static com.example.finddamatch.UI.GameActivity.score;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.finddamatch.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class Won extends AppCompatActivity {
 
@@ -23,6 +29,17 @@ public class Won extends AppCompatActivity {
         SharedPreferences.Editor editor=preferences.edit();
         editor.putInt("Sixth",score);
         editor.apply();
+
+        SharedPreferences date=getSharedPreferences("date",0);
+        SharedPreferences.Editor dateEdit = date.edit();
+
+        //https://stackoverflow.com/questions/8654990/how-can-i-get-current-date-in-android#:~:text=on%20this%20post.-,Date%20c%20%3D%20Calendar.getInstance().getTime()%3B%20System.,%3D%20df.format(c)%3B
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+        String formattedDate = df.format(c);
+        Log.d("haz",formattedDate);
+        dateEdit.putString("date 6",formattedDate);
+        dateEdit.apply();
 
         TextView scoreView= findViewById(R.id.winText);
         scoreView.setText("Congratulations, you won! Your time is "+(float)score/1000+" ms.");

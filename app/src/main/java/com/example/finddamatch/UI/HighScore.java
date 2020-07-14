6a56f3f1c60702c2fda23e74214da7a78b05389e
xtran <xuan_anh_tran@sfu.ca>
2020-07-14 10:15:45 +0700

    timestamp for highscore

--- app/src/main/java/com/example/finddamatch/UI/HighScore.java
+++ app/src/main/java/com/example/finddamatch/UI/HighScore.java
@@ -10,6 +10,11 @@ import android.widget.TextView;
 
 import com.example.finddamatch.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class HighScore extends AppCompatActivity {
 
     public static Intent makeLaunchIntent(Context c) {
@@ -32,12 +37,27 @@ public class HighScore extends AppCompatActivity {
         fifth = preferences.getInt("Fifth", 20000);
         sixth = preferences.getInt("Sixth", 20000);
 
-
         SharedPreferences.Editor editor = preferences.edit();
+
+        String date1, date2, date3, date4, date5, date6;
+
+        SharedPreferences date = getSharedPreferences("date", 0);
+        date1 = date.getString("date1", "10-Jul-2020");
+        date2 = date.getString("date2", "10-Jul-2020");
+        date3 = date.getString("date3", "10-Jul-2020");
+        date4 = date.getString("date4", "10-Jul-2020");
+        date5 = date.getString("date5", "10-Jul-2020");
+        date6 = date.getString("date6", "10-Jul-2020");
+        SharedPreferences.Editor dateEdit = date.edit();
+
         if (sixth < fifth) {
             fifth = sixth;
             editor.putInt("Fifth", fifth);
             editor.apply();
+
+            date5 = date6;
+            dateEdit.putString("date5", date5);
+            dateEdit.apply();
         }
         if (sixth < fourth) {
             int temp = fourth;
@@ -46,6 +66,13 @@ public class HighScore extends AppCompatActivity {
             editor.putInt("Fifth", fifth);
             editor.putInt("Fourth", fourth);
             editor.apply();
+
+            String dateTemp = date4;
+            date4 = date6;
+            date5 = dateTemp;
+            dateEdit.putString("date5", date5);
+            dateEdit.putString("date4", date4);
+            dateEdit.apply();
         }
         if (sixth < third) {
             int temp = third;
@@ -54,6 +81,13 @@ public class HighScore extends AppCompatActivity {
             editor.putInt("Third", third);
             editor.putInt("Fourth", fourth);
             editor.apply();
+
+            String dateTemp = date3;
+            date3 = date6;
+            date4 = dateTemp;
+            dateEdit.putString("date4", date4);
+            dateEdit.putString("date3", date3);
+            dateEdit.apply();
         }
         if (sixth < second) {
             int temp = second;
@@ -62,6 +96,13 @@ public class HighScore extends AppCompatActivity {
             editor.putInt("Second", second);
             editor.putInt("Third", third);
             editor.apply();
+
+            String dateTemp = date2;
+            date2 = date6;
+            date3 = dateTemp;
+            dateEdit.putString("date2", date2);
+            dateEdit.putString("date3", date3);
+            dateEdit.apply();
         }
         if (sixth < first) {
             int temp = first;
@@ -70,14 +111,22 @@ public class HighScore extends AppCompatActivity {
             editor.putInt("First", first);
             editor.putInt("Second", second);
             editor.apply();
+
+            String dateTemp = date4;
+            date1 = date6;
+            date2 = dateTemp;
+            dateEdit.putString("date1", date1);
+            dateEdit.putString("date2", date2);
+            dateEdit.apply();
         }
+
 
         editor.putInt("Sixth", 60000);
         editor.apply();
-        score.setText("First: " + (float) first/1000 + "\n" +
-                "Second: " + (float) second/1000 + "\n" +
-                "Third: " + (float) third/1000 + "\n" +
-                "Fourth: " + (float) fourth/1000 + "\n" +
-                "Fifth: " + (float) fifth/1000 + "\n");
+        score.setText("First: " + (float) first / 1000 + " - " + date1 + " " + "\n" +
+                "Second: " + (float) second / 1000 + " - " + date2 + " " + "\n" +
+                "Third: " + (float) third / 1000 + " - " + date3 + " " + "\n" +
+                "Fourth: " + (float) fourth / 1000 + " - " + date4 + " " + "\n" +
+                "Fifth: " + (float) fifth / 1000 + " - " + date5 + " " + "\n");
     }
 }
--- app/src/main/java/com/example/finddamatch/UI/Won.java
+++ app/src/main/java/com/example/finddamatch/UI/Won.java
@@ -6,12 +6,18 @@ import static com.example.finddamatch.UI.GameActivity.score;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.finddamatch.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class Won extends AppCompatActivity {
     private EditText userNameEntered;
@@ -25,6 +31,16 @@ public class Won extends AppCompatActivity {
         SharedPreferences.Editor editor=preferences.edit();
         editor.putInt("Sixth",score);
         editor.apply();
+
+        SharedPreferences date=getSharedPreferences("date",0);
+        SharedPreferences.Editor dateEdit = date.edit();
+
+        //https://stackoverflow.com/questions/8654990/how-can-i-get-current-date-in-android#:~:text=on%20this%20post.-,Date%20c%20%3D%20Calendar.getInstance().getTime()%3B%20System.,%3D%20df.format(c)%3B
+        String formattedDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        Log.d("haz",formattedDate);
+        dateEdit.putString("date 6",formattedDate);
+        dateEdit.apply();
+
         TextView scoreView= findViewById(R.id.winText);
         scoreView.setText("Congratulations, you won! Your time is "+(float)score/1000+" ms.");
         updateUI();

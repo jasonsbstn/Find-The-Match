5e32f5b99b1f8ef457e5ddf86ea83d2fbfdc7337
Jason Sebastian <jaritant@sfu.ca>
2020-07-23 13:18:44 -0700

    start flickr

---
+++ app/src/main/java/com/example/finddamatch/flickr/PhotoGalleryActivity.java
@@ -0,0 +1,12 @@
+package com.example.finddamatch.flickr;
+
+
+import androidx.fragment.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
